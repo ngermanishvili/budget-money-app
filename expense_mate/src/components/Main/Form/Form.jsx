21c39b584c2fbeb6df8e9b9f14536feb92e3f257
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-12 18:52:31 +0400

    Take information from inputs with using useState

--- expense_mate/src/components/Main/Form/Form.jsx
+++ expense_mate/src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   TextField,
   Typography,
@@ -11,10 +11,20 @@ import {
 } from "@material-ui/core";
 
 import useStyles from "./styles";
+// # I make this initialState for the form data for example amount, category, type, date etc and we are passing the initialState as the initial value and we are destructuring the formData and setFormData from the useState. for example if we want to change the amount we can do it like this setFormData({ ...formData, amount: 10 }) and this will change the amount to 10 and the rest of the data will remain the same.
+const initialState = {
+  amuont: "",
+  category: "",
+  type: "Income",
+  date: new Date(),
+};
 
 const Form = () => {
   const classes = useStyles();
+  // # This usestate is for the form data for example amount, category, type, date etc and we are passing the initialState as the initial value and we are destructuring the formData and setFormData from the useState.
+  const [formData, setFormData] = useState(initialState);
 
+  console.log(formData);
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -25,7 +35,10 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Type</InputLabel>
-          <Select>
+          <Select
+            value={formData.type}
+            onChange={(e) => setFormData({ ...formData, type: e.target.value })}
+          >
             <MenuItem value="Income">Income</MenuItem>
             <MenuItem value="Expense">Expense</MenuItem>
           </Select>
@@ -34,17 +47,34 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Category</InputLabel>
-          <Select>
+          <Select
+            value={formData.category}
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <MenuItem value="Business">Business</MenuItem>
             <MenuItem value="Salary">Salary</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type="number" label="Amount" fullWidth />
+        <TextField
+          type="number"
+          label="Amount"
+          fullWidth
+          value={formData.amuont}
+          onChange={(e) => setFormData({ ...formData, amuont: e.target.value })}
+        />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="Date" label="Date" fullWidth />
+        <TextField
+          type="Date"
+          label="Date"
+          fullWidth
+          value={formData.date}
+          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+        />
       </Grid>
       <Button
         className={classes.button}
