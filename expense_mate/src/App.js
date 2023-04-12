96ed77fc95734f300f5d0fdee3c32f33d223c824
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-12 17:51:50 +0400

    Import Grid Logic from materialUI

--- expense_mate/src/App.js
+++ expense_mate/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
     return (
         <div>
-            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh' }}>
 
                 <Grid item xs={12} sm={4} >
                     <Details title="Income" />
@@ -22,7 +22,6 @@ const App = () => {
                 <Grid item xs={12} sm={4}>
                     <Details title="Expense" />
                 </Grid>
-
             </Grid>
         </div>
     )
