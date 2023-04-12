65ea42db72c623627850f465703eeb8d0ecc35fb
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-12 19:27:38 +0400

    Added categories styles to constant folder

--- expense_mate/src/components/Main/Form/Form.jsx
+++ expense_mate/src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import {
   TextField,
   Typography,
@@ -9,11 +9,13 @@ import {
   Select,
   MenuItem,
 } from "@material-ui/core";
-
+import { ExpenseTrackerContext } from "../../../context/context";
 import useStyles from "./styles";
+import { v4 as uuidv4 } from "uuid";
+
 // # I make this initialState for the form data for example amount, category, type, date etc and we are passing the initialState as the initial value and we are destructuring the formData and setFormData from the useState. for example if we want to change the amount we can do it like this setFormData({ ...formData, amount: 10 }) and this will change the amount to 10 and the rest of the data will remain the same.
 const initialState = {
-  amuont: "",
+  amount: "",
   category: "",
   type: "Income",
   date: new Date(),
@@ -23,8 +25,22 @@ const Form = () => {
   const classes = useStyles();
   // # This usestate is for the form data for example amount, category, type, date etc and we are passing the initialState as the initial value and we are destructuring the formData and setFormData from the useState.
   const [formData, setFormData] = useState(initialState);
+  const { addTransaction } = useContext(ExpenseTrackerContext);
 
-  console.log(formData);
+  
+  const createTransaction = () => {
+    const transaction = {
+      ...formData,
+      amount: Number(formData.amount),
+      id: uuidv4(),
+    };
+    //! #  This addTransaction(transaction) is coming from the context and we are passing the transaction as the argument and this will add the transaction to the global state. for example if we want to add the transaction to the incomeTransactions we can do it like this addTransaction({ ...transaction, type: "Income" }) and this will add the transaction to the incomeTransactions.
+    addTransaction(transaction);
+    //! # This setFormData(initialState) is for resetting the form data to the initialState.
+    setFormData(initialState);
+  };
+
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -64,7 +80,7 @@ const Form = () => {
           label="Amount"
           fullWidth
           value={formData.amuont}
-          onChange={(e) => setFormData({ ...formData, amuont: e.target.value })}
+          onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
         />
       </Grid>
       <Grid item xs={6}>
@@ -81,6 +97,7 @@ const Form = () => {
         variant="outlined"
         color="primary"
         fullWidth
+        onClick={createTransaction}
       >
         Create
       </Button>
